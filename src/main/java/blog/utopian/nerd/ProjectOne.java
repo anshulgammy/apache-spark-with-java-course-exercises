d43cc30f7f4dbc85387f8f6ffc56bae850dfdb76
Anshul Gautam <anshulgammy@gmail.com>
2025-04-01 23:06:55 +0530

    Added ProjectOne changes

--- src/main/java/blog/utopian/nerd/ProjectOne.java
+++ src/main/java/blog/utopian/nerd/ProjectOne.java
@@ -1,17 +1,34 @@
 package blog.utopian.nerd;
 
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-
+// Note: Add '--add-exports java.base/sun.nio.ch=ALL-UNNAMED' in VM options to run these programs on
+// JDK 17 and above.
 public class ProjectOne {
 
-    public static void main(String[] args) {
-        SparkSession sparkSession = SparkSession.builder().appName("CSV-To-DB").master("local").getOrCreate();
+  public static void main(String[] args) {
 
-        Dataset<Row> dataset = sparkSession.read().format("csv").option("header", true).load("src/main/resources/name-comments.txt");
+    SparkSession sparkSession =
+        SparkSession.builder().appName("CSV-To-DB").master("local").getOrCreate();
 
-        dataset.show();
-    }
+    Dataset<Row> dataset =
+        sparkSession
+            .read()
+            .format("csv")
+            .option("header", true)
+            .load("src/main/resources/name-comments.txt");
+
+    dataset =
+        dataset
+            .filter(dataset.col("comment").rlike("\\d+"))
+            .withColumn(
+                "full_name", concat(dataset.col("first_name"), lit(" "), dataset.col("last_name")));
+
+    dataset.show();
+  }
 }
