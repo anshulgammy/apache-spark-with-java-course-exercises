17688a42619d5e64ac6507e60b2635460701fe33
Anshul Gautam <anshulgammy@gmail.com>
2025-04-03 18:51:33 +0530

    Update ProjectOne.java

--- src/main/java/blog/utopian/nerd/ProjectOne.java
+++ src/main/java/blog/utopian/nerd/ProjectOne.java
@@ -5,6 +5,7 @@ import static org.apache.spark.sql.functions.lit;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
 // Note: Add '--add-exports java.base/sun.nio.ch=ALL-UNNAMED' in VM options to run these programs on
@@ -13,9 +14,11 @@ public class ProjectOne {
 
   public static void main(String[] args) {
 
+    // Step 1: Create SparkSession.
     SparkSession sparkSession =
         SparkSession.builder().appName("CSV-To-DB").master("local").getOrCreate();
 
+    // Step 2: Read the data from source csv.
     Dataset<Row> dataset =
         sparkSession
             .read()
@@ -23,12 +26,23 @@ public class ProjectOne {
             .option("header", true)
             .load("src/main/resources/name-comments.txt");
 
+    // Step 3: Do transformation on dataset.
     dataset =
         dataset
             .filter(dataset.col("comment").rlike("\\d+"))
             .withColumn(
                 "full_name", concat(dataset.col("first_name"), lit(" "), dataset.col("last_name")));
 
+    // Step 4: Print the transformed data to console.
     dataset.show();
+
+    // Step 5: Write the transformed data to into output csv file.
+    dataset
+        .write()
+        .format("csv")
+        .option("header", true)
+        .mode(SaveMode.Overwrite)
+        .save(
+            "/Users/anshulgautam/Projects/Codebase/apache-spark-with-java-course-exercises/apache-spark-with-java-course-exercises/output");
   }
 }
